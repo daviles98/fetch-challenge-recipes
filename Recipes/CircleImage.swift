f1b1cd91951c1fe0f028c387d513a69aa2393fc8
Diego Aviles <diegoaviles1998@gmail.com>
2024-06-26 15:19:57 -0400

    create and combine views

---
+++ Recipes/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Recipes
+//
+//  Created by Diego 2 on 6/26/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
